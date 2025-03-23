1acea8b69895f3969b81ece6c10b81fae8bc51a6
asalmohammadjafari <asalmohammadjafarim@gmail.com>
2025-03-23 23:54:08 +0000

    Deploying to gh-pages from @ asalmohammadjafari/asalmohammadjafari.github.io@e734bf71e03e0ae61aa3ba83f4c7b939fc39ff21 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "To view the full CV, click the PDF download button.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
@@ -452,6 +452,13 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://www.linkedin.com/in/asalmohammadjafari", "_blank");
+        },
+      },{
+        id: 'social-telegram',
+        title: 'telegram',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://telegram.me/asalmjm", "_blank");
         },
       },{
       id: 'light-theme',
